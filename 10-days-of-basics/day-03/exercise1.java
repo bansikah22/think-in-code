72984ed998747fc25a4a92ab7c59f71fa34e28ea
bansikah22 <tandapnoelbansikah@gmail.com>
2025-08-25 19:42:26 +0100

    Complete Day 1 Exercise 3 Java

--- 10-days-of-basics/day-03/exercise1.java
+++ 10-days-of-basics/day-03/exercise1.java
@@ -4,21 +4,34 @@ public class Exercise1 {
         
         // TODO: Create strings
         // 1. Create a String variable named 'firstName' and assign it the value "John"
+        String firstName = "John";
         // 2. Create a String variable named 'lastName' and assign it the value "Doe"
+        String lastName = "Doe";
         
         // TODO: Perform basic operations
         // 1. Concatenate firstName and lastName with a space in between, store in 'fullName'
+        String fullName = firstName + " "+ lastName;
         // 2. Get the length of fullName and store in 'nameLength'
+        int nameLength = fullName.length();
+        
         // 3. Get the first character of fullName and store in 'firstChar'
+        char firstChar = fullName.charAt(0);
         // 4. Get the last character of fullName and store in 'lastChar'
+        char lastChar = fullName.charAt(fullName.length() - 1);
         
         // TODO: Display results
         // 1. Print "First name: " followed by firstName
+        System.out.println("First name: " + firstName);
         // 2. Print "Last name: " followed by lastName
+        System.out.println("lastName name: " + firstName);
         // 3. Print "Full name: " followed by fullName
+        System.out.println("Full name:" + fullName);
         // 4. Print "Name length: " followed by nameLength
+       System.out.println("Name length: " + nameLength);
         // 5. Print "First character: " followed by firstChar
+        System.out.println("First character: " + firstChar);
         // 6. Print "Last character: " followed by lastChar
+        System.out.println("Last character: " + lastChar);
         
         // Expected output:
         // First name: John
