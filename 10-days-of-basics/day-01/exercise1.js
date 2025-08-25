ef90602e09d567da5ac6b23a27074ce62344a60c
rdala <rodriguefohforingwe@gmail.com>
2025-08-24 20:07:42 -0400

    feat:completed day 1 exercise of 10 days challenge

--- 10-days-of-basics/day-01/exercise1.js
+++ 10-days-of-basics/day-01/exercise1.js
@@ -1,19 +1,27 @@
-// Exercise 1: Variable Declaration and Assignment
-
+// Exercise 1: Variable Declaration and Assignment 
 // TODO: Declare variables for different data types
 // 1. Create a const variable named 'name' and assign it the value "John Doe"
+ const name ="John Doe";
 // 2. Create a const variable named 'age' and assign it the value 25
+const age =25;
 // 3. Create a const variable named 'height' and assign it the value 5.9
+const height =5.9;
 // 4. Create a const variable named 'isStudent' and assign it the value true
+const isStudent =true;
 // 5. Create a const variable named 'favoriteColors' and assign it an array with values "red", "blue", "green"
+const favoriteColors =["red", "blue", "green"];
 
 // TODO: Display the variables
 // 1. Use console.log to print "Name: " followed by the name variable
+console.log('name: '  + name);
 // 2. Use console.log to print "Age: " followed by the age variable
+console.log('age: '  + 25);
 // 3. Use console.log to print "Height: " followed by the height variable
+console.log('height: '  + 5.9);
 // 4. Use console.log to print "Is Student: " followed by the isStudent variable
+console.log('isStudent: ' + true);
 // 5. Use console.log to print "Favorite Colors: " followed by the favoriteColors array
-
+console.log("favoriteColors: "  + favoriteColors);
 // Expected output:
 // Name: John Doe
 // Age: 25
