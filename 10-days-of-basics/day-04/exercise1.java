4b10433758634a29089cf7e9c4a1dc2d0d9fac85
Noel Bansikah <tandapnoelbansikah@gmail.com>
2025-08-26 15:45:23 +0100

    Completed excercise 1 day 4 10-days of basics

--- 10-days-of-basics/day-04/exercise1.java
+++ 10-days-of-basics/day-04/exercise1.java
@@ -4,19 +4,29 @@ public class Exercise1 {
         
         // TODO: Declare a number
         // 1. Create an int variable named 'number' and assign it the value 15
+        int number = 15;
         
         // TODO: Check various conditions
         // 1. Check if number is positive (> 0) and store result in 'isPositive'
+        boolean isPositive = number > 0;
         // 2. Check if number is even (use modulo operator %) and store result in 'isEven'
+        boolean isEven = (number % 2 == 0);
         // 3. Check if number is greater than 10 and store result in 'isGreaterThan10'
+        boolean isGreaterThan10 = number > 10;
         // 4. Check if number is less than 20 and store result in 'isLessThan20'
+        boolean isLessThan20 = number < 20;
         
         // TODO: Display results
         // 1. Print "Number: " followed by the number
+        System.out.println("Number: "+ number);
         // 2. Print "Is positive: " followed by isPositive
+        System.out.println("Is positive: "+ isPositive);
         // 3. Print "Is even: " followed by isEven
+        System.out.println("Is even: "+ isEven);
         // 4. Print "Is greater than 10: " followed by isGreaterThan10
+        System.out.println("Is greater than 10: "+ isGreaterThan10);
         // 5. Print "Is less than 20: " followed by isLessThan20
+        System.out.println("Is less than 20: "+ isLessThan20);
         
         // Expected output:
         // Number: 15
@@ -24,5 +34,5 @@ public class Exercise1 {
         // Is even: false
         // Is greater than 10: true
         // Is less than 20: true
-    }
+   }
 }
