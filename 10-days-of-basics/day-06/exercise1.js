d7d4425c6995492c5a3a75f8acb1f698f11839e0
rdala <rodriguefohforingwe@gmail.com>
2025-08-28 21:44:16 -0400

    feat: completed exercise1.js of day 6 of the 10 day challenge

--- 10-days-of-basics/day-06/exercise1.js
+++ 10-days-of-basics/day-06/exercise1.js
@@ -2,19 +2,28 @@
 
 // TODO: Create and manipulate an array
 // 1. Create a const variable named 'fruits' and assign it an array with initial values "apple", "banana"
+const fruits =[' apple ', '  banana '];
 // 2. Use console.log to print the original array
-
+console.log('Original array:  ' + fruits);
 // TODO: Add elements
 // 1. Add "orange" to the end of the array using push()
+fruits.push(' orange ');
 // 2. Use console.log to print the array after adding orange
+console.log('After adding oringe:  ' + fruits);
 // 3. Add "grape" at the beginning using unshift()
+let newLength = fruits.unshift(' grape ');
 // 4. Use console.log to print the array after adding grape
+console.log('After adding grape at the biginning:  ' + fruits);
 
 // TODO: Remove elements
 // 1. Remove the last element using pop()
+fruits.pop(' orange ');
 // 2. Use console.log to print the array after removing last element
+console.log('After removing the last element:  ' + fruits);
 // 3. Remove the first element using shift()
+fruits.shift('grape');
 // 4. Use console.log to print the array after removing first element
+console.log('After removing the first element:  ' + fruits);
 
 // Expected output:
 // Original array: [ 'apple', 'banana' ]
